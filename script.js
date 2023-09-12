e9f91d5e46d8714f9f786bc19b5ce4b249fa19a1
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-09-12 14:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 function decimalToBinary(num) {
   //Write you code here
-	# Function to convert decimal to binary
-def decimal_to_binary(decimal):
-    # Using the bin() function to convert to binary and stripping the '0b' prefix
-    binary = bin(decimal)[2:]
-    return binary
-
-# Input from the user
-decimal_input = int(input("Enter a decimal number: "))
-
-# Convert the decimal to binary
-binary_output = decimal_to_binary(decimal_input)
-
-# Print the binary equivalent
-print("Binary equivalent:", binary_output)
-
-  
-}
+	decimalToBinary(input)=output_decimalToBinary
+	{
 
 window.decimalToBinary = decimalToBinary;
